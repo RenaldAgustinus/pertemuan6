55e2f070231d85fe3e94d3af2195c375899945bc
renaldAgustinus <renaldagustinus@gmail.com>
2025-10-03 10:44:47 +0700

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,26 @@ class RenaldApp extends StatelessWidget {
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const<Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              ListTile(leading: Icon(Icons.message),title: Text("Messages")),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text("Profile"),
+              ),
+              ListTile(leading: Icon(Icons.settings),title: Text("Settings")),
+            ],
+          ),
+        ),
       ),
     );
   }
