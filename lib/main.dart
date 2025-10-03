4d27f52b30452be26cb6211d75b0f923f4a579ea
renaldAgustinus <renaldagustinus@gmail.com>
2025-10-03 10:45:40 +0700

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,16 @@ class RenaldApp extends StatelessWidget {
             ],
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const<BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business' 
+            ),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+          ],
+        ),
       ),
     );
   }
